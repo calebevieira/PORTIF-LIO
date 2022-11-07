4744483cafebbfe59a39defa99103da7b84312a3
calebevieira <calebetokyo@gmail.com>
2022-11-07 17:57:25 -0300

    .
    .

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ const Sidebar = () => {
       <p className="title">Desenvolvedor Full Stack</p>
       <SocialNetworkContainer />
       <InformationContainer />
-      <a href="calebe.pdf" className="btn" download>Baixar currículo</a>
+      <a href="https://drive.google.com/file/d/1eCq4ysZhhsr_KzJzmdstd55khEF8xKn5/view?usp=share_link" className="btn" download>Baixar currículo</a>
     </aside>
   );
 };
