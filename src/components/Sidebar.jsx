fc3b5b616e26964b992b80fc52ca72858909d475
calebevieira <calebetokyo@gmail.com>
2022-11-07 17:35:12 -0300

    .
    .

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ const Sidebar = () => {
       <p className="title">Desenvolvedor Full Stack</p>
       <SocialNetworkContainer />
       <InformationContainer />
-      <a href="/calebe.pdf" className="btn" download>Baixar currículo</a>
+      <a href="/calebe.rar" className="btn" download>Baixar currículo</a>
     </aside>
   );
 };
